6c691cb912e6015150a2821214a5d43313870f9a
Kirill Karaganov <kmkaraganov@gmail.com>
2026-08-04 21:47:14 +0300

    feat: config added

--- src/auth_service/database/models.py
+++ src/auth_service/database/models.py
@@ -2,4 +2,21 @@ from datetime import datetime
 
 from sqlalchemy import Integer, ForeignKey, DateTime, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .database import Base
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key = True, autoincrement=True)
+
+
+class rolls(Base):
+    pass 
+
+class user_roles(Base):
+    pass
